341420c98bbb54345db24cd4faa2533ba92b8045
Your Name <you@example.com>
2020-01-30 12:54:38 -0800

    custom_len done

--- further_study.py
+++ further_study.py
@@ -30,8 +30,11 @@ def custom_len(input_list):
         8
 
     """
+    total = 0
+    for item in input_list:
+        total += 1
 
-    return 0
+    return total
 
 
 # For the next four exercises, you'll need to be clever and think about ways
